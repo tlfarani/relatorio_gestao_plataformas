34ec213962c48cc7b7f09ef1f7c8e06567b56446
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-27 15:05:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ MULTIPLICADOR_FONTE = 1
 # Configuração da imagem que será salva ao clicar no ícone de câmera (download)
 CONFIG_EXPORTACAO = {
     'toImageButtonOptions': {
-        'format': 'svg',            # Pode alterar para 'svg' se quiser gráficos vetoriais no Word
+        'format': 'jpg',            # Pode alterar para 'svg' se quiser gráficos vetoriais no Word
         'filename': 'grafico_ibama',
         'height': 650,               # Altura fixa para o documento
         'width': 1200,               # Largura fixa para o documento
