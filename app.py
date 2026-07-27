6b9a5494ca38f5feb0594a71b4ab3698853a6b42
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-27 16:14:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -401,8 +401,8 @@ if os.path.exists(NOME_ACIDENTES) and os.path.exists(NOME_PRODUCAO) and os.path.
                     legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5, font=dict(size=15), entrywidth=105, entrywidthmode="pixels"), 
                     margin=dict(t=100, b=50, l=50, r=50)
                 )
-                fig2.update_xaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=15.6))
-                fig2.update_yaxes(range=[0, max_y * 1.15], showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=15.6))
+                fig2.update_xaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=18))
+                fig2.update_yaxes(range=[0, max_y * 1.15], showgrid=False, zeroline=False, linecolor='black', title_font=dict(size=18, color='black'), tickfont=dict(size=15.6))
                 
                 st.plotly_chart(ajustar_layout_grafico(fig2, espessura_barra=0.85), use_container_width=True, config=CONFIG_EXPORTACAO)
                 
