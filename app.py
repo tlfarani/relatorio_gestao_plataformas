62283dd6828e33df2f0e33b03b23cbb5182c92be
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-27 15:41:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -394,7 +394,9 @@ if os.path.exists(NOME_ACIDENTES) and os.path.exists(NOME_PRODUCAO) and os.path.
                 fig2.update_traces(textposition='outside', textfont=dict(color='black', size=14.4))
                 fig2.update_layout(
                     xaxis_title="", yaxis_title="Nº de Acidentes", plot_bgcolor='white', paper_bgcolor='white', font=dict(color='black', size=15.6),
-                    legend_title_text='', legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5), margin=dict(t=100, b=50, l=50, r=50)
+                    legend_title_text='', 
+                    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5, font=dict(size=15.6), entrywidth=210, entrywidthmode="pixels"), 
+                    margin=dict(t=100, b=50, l=50, r=50)
                 )
                 fig2.update_xaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=14.4))
                 fig2.update_yaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=14.4))
