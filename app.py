a4691cf7a3122b083490af5019b8ce9934e3abb2
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-27 15:01:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import io
 # =========================================================================
 # 1. CONFIGURAÇÕES DE VISUALIZAÇÃO E EXPORTAÇÃO DOS GRÁFICOS
 # =========================================================================
-MULTIPLICADOR_FONTE = 1.2 
+MULTIPLICADOR_FONTE = 1
 
 # Configuração da imagem que será salva ao clicar no ícone de câmera (download)
 CONFIG_EXPORTACAO = {
