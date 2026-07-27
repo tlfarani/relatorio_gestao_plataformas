a663c5818a73f8c71370e052d3b5d5f7ccbdc2bf
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-27 17:08:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -445,8 +445,8 @@ if os.path.exists(NOME_ACIDENTES) and os.path.exists(NOME_PRODUCAO) and os.path.
                 fig4.update_layout(
                     xaxis_title="", yaxis_title="Acidentes a cada Mboe/d", plot_bgcolor='white', paper_bgcolor='white', font=dict(color='black', size=15.6),
                     legend_title_text='', 
-                    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5,  font=dict(size=15.6), entrywidth=105, entrywidthmode="pixels", 
-                    margin=dict(t=100, b=50, l=50, r=50)))
+                    legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5,  font=dict(size=15.6), entrywidth=105, entrywidthmode="pixels"), 
+                    margin=dict(t=100, b=50, l=50, r=50))
                 fig4.update_xaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(size=15.6, color = 'black'))
                 fig4.update_yaxes(showgrid=False, showticklabels=False, zeroline=False, linecolor='black',  title_font=dict(size=15.6, color='black'), tickfont=dict(size=15.6, color='black'))
                 st.plotly_chart(ajustar_layout_grafico(fig4), use_container_width=True, config=CONFIG_EXPORTACAO)
