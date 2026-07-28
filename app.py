4b04671124638e9a736c59780d089dec023b152d
Tiago Farani <36167783+tlfarani@users.noreply.github.com>
2026-07-28 10:51:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -707,7 +707,7 @@ if os.path.exists(NOME_ACIDENTES) and os.path.exists(NOME_PRODUCAO) and os.path.
                     margin=dict(t=50, b=50, l=50, r=50)
                 )
                 fig8.update_xaxes(showgrid=False, zeroline=False, linecolor='black', tickfont=dict(color='black', size=15))
-                fig8.update_yaxes(title_text="Número de Processos", 
+                fig8.update_yaxes(title_text="", 
                                   showticklabels=False, showgrid=False, zeroline=False, showline=False, 
                                   title_font=dict(color='black', size=15))
                 st.plotly_chart(ajustar_layout_grafico(fig8), use_container_width=True, config=CONFIG_EXPORTACAO)
